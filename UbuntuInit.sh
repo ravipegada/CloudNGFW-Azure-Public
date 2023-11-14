65d412d63db4beab5add61ba27365bdb025bfbac
ravipegada <100585753+ravipegada@users.noreply.github.com>
2023-11-14 21:57:42 +0530

    Update UbuntuInit.sh

--- UbuntuInit.sh
+++ UbuntuInit.sh
@@ -4,9 +4,9 @@ sudo apt update
 # apache installation, enabling and status check
 sudo apt -y install apache2
 # Copy files to showcase security use case
-sudo wget -P /www/var/html/ https://github.com/ravipegada/CNGFW-Azure/blob/main/web-to-db-attack.html
-sudo wget -p /www/var/html/ https://github.com/ravipegada/CNGFW-Azure/blob/main/sql-attack.html
-sudo wget -p /www/var/html/ https://github.com/ravipegada/CNGFW-Azure/blob/main/showheaders.php
-sudo wget -p /www/var/html/ https://github.com/ravipegada/CNGFW-Azure/blob/main/index.html
+sudo wget -P /var/www/html/ https://github.com/ravipegada/CNGFW-Azure/blob/main/web-to-db-attack.html
+sudo wget -p /var/www/html/ https://github.com/ravipegada/CNGFW-Azure/blob/main/sql-attack.html
+sudo wget -p /var/www/html/ https://github.com/ravipegada/CNGFW-Azure/blob/main/showheaders.php
+sudo wget -p /var/www/html/ https://github.com/ravipegada/CNGFW-Azure/blob/main/index.html
 sudo wget -p /usr/lib/cgi-bin/ https://github.com/ravipegada/CNGFW-Azure/blob/main/guess-sql-root-password.cgi
 sudo service apache2 restart
